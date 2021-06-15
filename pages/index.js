2c865cbee0ac2872bb5a834807b1a6c89c954fbc
Ch1l4v3r7 <muhammad.chilavert@gmail.com>
2021-06-15 13:58:05 +0200

    use fragment as a page container instaed of div

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Casita</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
-    </div>
+    </>
   )
 }
